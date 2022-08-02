d4bdd6cfefc771f7496c39a7a0c8f9c8a0bb53b5
tzadoque <zadoque.dev@gmail.com>
2022-08-01 23:25:55 -0300

    feat(index): Finalizando o form de solicitação de serviço

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 const form = document.querySelector('#form-servicos');
 
+const buttons = form.querySelectorAll('button:not(button[type="submit"])');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+});
+
 const steps = form.querySelectorAll('[data-steps]');
 
 const nextStep = (current, next) => {
@@ -10,14 +18,16 @@ const nextStep = (current, next) => {
     steps[next].classList.remove('d-none');
     steps[next].classList.add('d-flex');
   }, 500);
+  form.scrollIntoView();
 };
 
 const prevStep = (prev, current) => {
   setTimeout(() => {
     steps[prev].classList.remove('d-none');
     steps[prev].classList.add('d-flex');
-    
+
     steps[current].classList.remove('d-flex');
     steps[current].classList.add('d-none');
   }, 500);
+  form.scrollIntoView();
 };
