faf47b04f51d96002c68b8595f5a9d34a733d612
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-10-21 02:14:19 -0300

    feat: adicionando a página de planos

--- page-planos.php
+++ page-planos.php
@@ -4,4 +4,219 @@
 
 <?php get_header(); ?>
 
+<main class="main-planos">
+  <div class="bg-img container">
+    <div class="container px-awe-24 px-lg-0 pt-awe-40 py-lg-awe-80 px-md-awe-128">
+      <div class="row">
+        <div class="col-12 col-lg-5 my-lg-80 my-xl-awe-128 mb-awe-32 mb-lg-0">
+          <h1 class="text-light-3 fz-28 fz-md-32 fz-lg-40 fw-bold border-bottom border-secondary border-4 pb-awe-8 d-inline-block mb-awe-48">
+            Revojus
+          </h1>
+
+          <p class="text-light-2 fz-16 fz-md-18 mb-awe-48">
+            O seu reconhecimento como advogado não passa pelo tamanho do seu escritório físico.
+            A Revojus nasceu para transformar a forma como você trabalha e com essa transformação esperamos que seus resultados na advocacia sejam impulsionados.
+          </p>
+
+          <a href="" class="btn btn-secondary rounded-1 px-awe-24 py-awe-12 fw-bold fz-16 text-light-2">
+            Quero fazer parte da Revojus
+          </a>
+        </div>
+        <div class="col-12 d-lg-none">
+          <img src="<?php echo get_template_directory_uri(); ?>/dist/img/img-planos-1.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<section>
+  <div class="container px-awe-24 px-lg-0 py-awe-80 py-lg-awe-128">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-10 d-flex flex-column gap-awe-40">
+        <div class="row">
+          <div class="d-none d-lg-flex col-lg-4 align-items-start justify-content-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/svg/groups.svg" class="img-fluid" alt="">
+          </div>
+          <div class="col-12 col-lg-8">
+            <p class="text-darker-1 fz-16 fz-md-18 mb-awe-40">
+              A Revojus está há 2 anos sendo desenvolvida por pessoas dedicadas em transformar a relação entre advogados e clientes. Hoje, no mundo tradicional, funciona assim: um cliente marca um horário com um advogado, chega em seu escritório, descreve seu problema e o profissional fecha ou não o acordo.
+            </p>
+            <p class="text-secondary fz-16 fz-md-18 fw-bold">
+              Mas a verdade é que esse não precisa ser o único caminho…
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="d-none d-lg-flex col-lg-4 align-items-start justify-content-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/svg/computer.svg" class="img-fluid" alt="">
+          </div>
+          <div class="col-12 col-lg-8">
+            <p class="text-darker-1 fz-16 fz-md-18 mb-awe-40">
+              Com o mundo cada vez mais digital, tudo pode ser feito de forma rápida e com menos custos para você e os clientes. Imagine você, na sua casa, em um escritório totalmente digital e com demandas que não param de chegar?
+            </p>
+            <p class="text-secondary fz-16 fz-md-18 fw-bold">
+              É exatamente isso que a Revojus propõe.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="d-none d-lg-flex col-lg-4 align-items-start justify-content-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/img/svg/business.svg" class="img-fluid" alt="">
+          </div>
+          <div class="col-12 col-lg-8">
+            <p class="text-darker-1 fz-16 fz-md-18">
+              Acreditamos que você não precisa pagar R$2000 ou até mais em um escritório físico. Não é com isso que o cliente se importa. O que cada cliente sonha é ter seu problema resolvido e você já tem o conhecimento necessário para isso.
+            </p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12 col-lg-4"></div>
+          <div class="col-12 col-lg-8">
+            <a href="" class="btn btn-secondary rounded-1 px-awe-24 py-awe-12 fw-bold fz-16 text-light-2">
+              Quero fazer parte da Revojus
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="bg-darker-1">
+  <div class="container px-awe-24 px-lg-0 py-awe-80 py-lg-awe-128">
+    <div class="row justify-content-center">
+      <div class="col-12 d-flex justify-content-center mb-awe-40">
+        <h2 class="text-light-3 fw-bold fz-21 fz-sm-28 fz-md-32 fz-lg-40 d-inline-block border-bottom border-secondary border-4 pb-awe-8">
+          Por que a Revojus?
+        </h2>
+      </div>
+      <div class="col-12 col-md-11 mb-awe-80 mb-lg-awe-96">
+        <p class="m-0 text-light-3 fw-semi-bold fz-21 fz-lg-24 text-center lh-140 ff-open-sans">
+          A nossa plataforma digital é para todos aqueles advogados que desejam ter uma carreira de sucesso, captar novos clientes e não gastar muito com um escritório físico.
+        </p>
+      </div>
+      <div class="col-12 col-md-10 col-lg-8 mb-awe-64">
+        <ul class="list-secondary text-light-3 fz-16 fz-md-18 fw-semi-bold d-flex flex-column gap-awe-40">
+          <li>
+            Em nosso sistema o advogado tem acesso a gerenciamento de demandas, clientes, calendário, rotinas, audiências… tudo que um escritório físico auxiliaria no trabalho de um advogado, mas de forma totalmente online.
+          </li>
+          <li>
+            E se o seu cliente precisar assinar algum documento, a Revojus através de uma tecnologia eficiente, solicita a assinatura digital. Ou seja, ele não precisa ir até você por causa de uma assinatura. Os dois ganham tempo.
+          </li>
+          <li>
+            Lembra que prometemos mais clientes para você atender? Todos os casos que chegam a Revojus serão repassados aos advogados cadastrados na plataforma. Você não precisa se preocupar com a prospecção.
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 d-flex justify-content-center">
+        <a href="" class="btn btn-secondary rounded-1 px-awe-24 py-awe-12 fw-bold fz-16 text-light-2">
+          Quero fazer parte da Revojus
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section style="background: linear-gradient(90deg, rgba(97, 22, 75, 0.25) 0%, rgba(97, 22, 75, 0) 100%), #D84026;">
+  <div class="container px-awe-24 px-lg-0 py-awe-80">
+    <div class="row justify-content-center">
+      <div class="col-12 mb-awe-24">
+        <h3 class="text-center text-light-3 fz-21 fz-md-32 fw-semi-bold">
+          Com essa tecnologia você será capaz de crescer na profissão e ter a sua carreira devidamente valorizada, da forma como você merece e sempre desejou.
+        </h3>
+      </div>
+      <div class="col-12 col-md-10 col-lg-8">
+        <p class="text-center text-light-3 fz-16 fz-md-18 fw-semi-bold">
+          Advogados são essenciais no Brasil e aqueles que trabalham com seriedade merecem ser valorizados pelo seus resultados.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="planos">
+  <div class="container px-awe-24 px-lg-0 py-awe-80 py-lg-awe-128">
+    <div class="row justify-content-center">
+      <div class="col-12 d-flex justify-content-center mb-awe-40">
+        <h4 class="text-main fw-bold fz-21 fz-md-32 fz-lg-40 d-inline-block border-bottom border-secondary border-4 pb-awe-8">
+          Selecione o melhor plano para você
+        </h4>
+      </div>
+
+      <div class="col-12 col-md-10 col-lg-8 mb-awe-96 d-flex justify-content-center">
+        <p class="fz-16 fz-md-18 text-darker-1 text-center">
+          Ao assinar um plano na Revojus, você tem acesso liberado a <strong>todas as tecnologias disponíveis</strong> da plataforma.
+          As informações de acesso chegarão até o seu email.
+        </p>
+      </div>
+
+      <div class="col-12 col-lg-10">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-5 justify-content-center">
+          <div class="col d-flex flex-column align-items-center">
+            <div class="card-plano mb-awe-40">
+              <span class="card-plano__tipo">
+                Mensal
+              </span>
+
+              <div class="card-plano__valor">
+                <span>
+                  <span class="fw-regular">R$</span>
+                  <span class="fw-bold">219</span>
+                </span>
+              </div>
+            </div>
+            <a href="" class="btn btn-secondary rounded-1 px-awe-24 py-awe-12 fw-bold fz-16 text-light-2">
+              Selecionar
+            </a>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <div class="card-plano mb-awe-40">
+              <span class="card-plano__tipo">
+                Trimestral
+              </span>
+
+              <div class="card-plano__valor">
+                <span>
+                  <span class="fw-regular">R$</span>
+                  <span class="fw-bold">264</span>
+                </span>
+
+                <div class="text-darker-1 fw-medium fz-21">
+                  ou 3x de R$88,00
+                </div>
+              </div>
+            </div>
+            <a href="" class="btn btn-secondary rounded-1 px-awe-24 py-awe-12 fw-bold fz-16 text-light-2">
+              Selecionar
+            </a>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <div class="card-plano mb-awe-40">
+              <span class="card-plano__tipo">
+                Anual
+              </span>
+
+              <div class="card-plano__valor">
+                <span>
+                  <span class="fw-regular">R$</span>
+                  <span class="fw-bold">816</span>
+                </span>
+
+                <div class="text-darker-1 fw-medium fz-21">
+                  ou 12x de R$68,00
+                </div>
+              </div>
+            </div>
+            <a href="" class="btn btn-secondary rounded-1 px-awe-24 py-awe-12 fw-bold fz-16 text-light-2">
+              Selecionar
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <?php get_footer(); ?>
